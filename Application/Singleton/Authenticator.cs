0e81fab9e50c4e2658052dd2ae0b0107b1eef26a
VadimSushinskiy <vadim0sushinsky@gmail.com>
2025-02-25 22:26:36 +0200

    Singleton

---
+++ Application/Singleton/Authenticator.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton
+{
+    public class Authenticator
+    {
+        private Authenticator() { }
+
+        private static Authenticator _instance = null;
+
+        private static readonly object _lock = new object();
+
+        public static Authenticator GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Authenticator();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Application/Singleton/Program.cs
@@ -0,0 +1,5 @@
+﻿using Singleton;
+
+Authenticator authenticator1 = Authenticator.GetInstance();
+Authenticator authenticator2 = Authenticator.GetInstance();
+Console.WriteLine(ReferenceEquals(authenticator1, authenticator2));
