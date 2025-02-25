1b64eba54457940496103a8fccc115aced0b9abc
VadimSushinskiy <vadim0sushinsky@gmail.com>
2025-02-25 22:43:45 +0200

    Prototype

---
+++ Application/Prototype/Program.cs
@@ -0,0 +1,17 @@
+﻿using Prototype;
+
+Virus virus1 = new Virus("Alex", 100, 20, "virus", []);
+Virus virus2 = new Virus("Alexx", 140, 23, "virus", []);
+Virus virus11 = new Virus("Bob", 1, 1, "virussuper", [virus1, virus2]);
+Virus virus12 = new Virus("Katy", 333, 234, "virussuper", []);
+Virus virus111 = new Virus("Kris", 55, 230, "father", [virus11, virus12]);
+
+Virus copy = virus111.Clone();
+
+Console.WriteLine(virus111.Childrens[0].Childrens[0].Name);
+Console.WriteLine(copy.Childrens[0].Childrens[0].Name);
+
+virus111.Childrens[0].Childrens[0].Name = "New Name";
+
+Console.WriteLine(virus111.Childrens[0].Childrens[0].Name);
+Console.WriteLine(copy.Childrens[0].Childrens[0].Name);
---
+++ Application/Prototype/Virus.cs
@@ -0,0 +1,43 @@
+﻿namespace Prototype
+{
+    public class Virus
+    {
+        public Virus(string name, int weight, int age, string type, Virus[] childrens)
+        {
+            Name = name;
+            Weight = weight;
+            Age = age;
+            Type = type;
+            Childrens = childrens;
+        }
+        public Virus() { }
+
+        public string Name { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Age { get; set; }
+
+        public string Type { get; set; }
+
+        public Virus[] Childrens { get; set; }
+
+        public Virus Clone()
+        {
+            Virus VirusCopy = new Virus();
+            VirusCopy.Name = Name;
+            VirusCopy.Weight = Weight;
+            VirusCopy.Age = Age;
+            VirusCopy.Type = Type;
+
+            VirusCopy.Childrens = new Virus[Childrens.Length];
+            for (int i = 0; i < Childrens.Length; i++)
+            {
+                VirusCopy.Childrens[i] = Childrens[i].Clone();
+            }
+
+
+            return VirusCopy;
+        }
+    }
+}
