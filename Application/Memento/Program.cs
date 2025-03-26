246cab697a0126f89bc81bf13c1c9893d39d3aab
VadimSushinskiy <vadim0sushinsky@gmail.com>
2025-03-26 20:36:02 +0200

    Added Memento

---
+++ Application/Memento/Program.cs
@@ -0,0 +1,20 @@
+﻿using Memento;
+
+TextEditor textEditor = new TextEditor();
+
+textEditor.AppendText("Hello");
+textEditor.Save();
+textEditor.AppendText(", I'm Steve!");
+textEditor.MoveCursorTo(4);
+textEditor.Save();
+textEditor.GetStateForTest();
+
+textEditor.WriteText("New text");
+textEditor.MoveCursorTo(1);
+textEditor.GetStateForTest();
+
+textEditor.Undo();
+textEditor.GetStateForTest();
+
+textEditor.Undo();
+textEditor.GetStateForTest();
---
+++ Application/Memento/TextEditor.cs
@@ -0,0 +1,68 @@
+﻿namespace Memento
+{
+    public class TextEditor
+    {
+        private readonly Stack<TextDocument> _changeHistory = new Stack<TextDocument>();
+
+        private string text = "";
+
+        private int cursorSeek;
+
+        public void AppendText(string newText)
+        {
+            text += newText;
+        }
+
+        public void WriteText(string newText)
+        {
+            text = newText;
+        }
+
+        public void MoveCursorTo(int newSeekPosition)
+        {
+            cursorSeek = newSeekPosition;
+        }
+
+        public void GetStateForTest()
+        {
+            Console.WriteLine($"Text: {text}, cursorSeek: {cursorSeek}");
+        }
+
+        public void Undo()
+        {
+            if (_changeHistory.Count > 0)
+            {
+                TextDocument textDocument = _changeHistory.Pop();
+                textDocument.Restore();
+            }
+        }
+
+        public void Save()
+        {
+            TextDocument textDocument = new TextDocument(this);
+            _changeHistory.Push(textDocument);
+        }
+
+        public class TextDocument
+        {
+            private string text;
+
+            private int cursorSeek;
+
+            private readonly TextEditor _textEditor;
+
+            public TextDocument(TextEditor textEditor)
+            {
+                _textEditor = textEditor;
+                text = _textEditor.text;
+                cursorSeek = _textEditor.cursorSeek;
+            }
+
+            public void Restore()
+            {
+                _textEditor.text = text;
+                _textEditor.cursorSeek = cursorSeek;
+            }
+        }
+    }
+}
