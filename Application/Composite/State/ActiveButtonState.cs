5a1c02b89cbf4f7a6057dce2ff9881bd93389537
VadimSushinskiy <vadim0sushinsky@gmail.com>
2025-04-14 17:24:26 +0300

    Add State

---
+++ Application/Composite/State/ActiveButtonState.cs
@@ -0,0 +1,25 @@
+﻿namespace Composite.State
+{
+    public class ActiveButtonState : ButtonState
+    {
+        public ActiveButtonState(LightButtonNode button) : base(button) { }
+
+        public override async void Click()
+        {
+            if (_button.ClickAction != null)
+            {
+                _button.SetState(new InactiveButtonState(_button));
+                await _button.ClickAction();
+                _button.SetState(new ActiveButtonState(_button));
+            }
+        }
+
+        public override async void Hover()
+        {
+            if (_button.HoverAction != null)
+            {
+                await _button.HoverAction();
+            }
+        }
+    }
+}
